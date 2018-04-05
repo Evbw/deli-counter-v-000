6b4bf2e33795beab85f8b8b26003c8738d3f24b6
Everett <everett.bowline@gmail.com>
2018-04-05 00:10:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name) 
+  katz_deli.push(name)
+  puts `Welcome, #{name}. You are number #{katz_deli.length} in line.`
+end
+
+def now_serving(katz_deli, name)
+  if (katz_deli.length > 0) 
+    puts `Currently serving #{katz_deli.shift()}.`
+  
+    else 
+      puts "There is nobody waiting to be served!"
+  end
+end
