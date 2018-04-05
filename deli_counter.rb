7b221661325d1eed3e1d284713875c906f4d78ba
Everett <everett.bowline@gmail.com>
2018-04-05 00:12:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ katz_deli = []
 def line(katz_deli) 
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    katz_deli.each("#{name} is currently #{katz_deli.[index]} in line")
   end
 end
 
